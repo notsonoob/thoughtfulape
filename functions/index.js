b7989e44f786bf6b0bc8bbe417d8b6f3a42e7643
Aditya Mehra <aditya97mehra@gmail.com>
2019-07-28 13:08:22 +0530

    gave users the abitlity to register and authentication changes

--- functions/index.js
+++ functions/index.js
@@ -1,18 +1,31 @@
 // firebase
+require("dotenv").config();
 const functions = require("firebase-functions");
 const admin = require("firebase-admin");
+const app = require("express")();
 
 // initializing the app
 admin.initializeApp();
 
-// express
-const express = require("express");
-const app = express();
+const firebaseConfig = {
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  databaseURL: process.env.DATABASE_URL,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID
+};
+
+// firebase npm
+const firebase = require("firebase");
+firebase.initializeApp(firebaseConfig);
+
+const db = admin.firestore();
 
 //to get all the screams
 app.get("/screams", (req, res) => {
-  admin
-    .firestore()
+  db.firestore()
     .collection("screams")
     .orderBy("createdAt", "desc")
     .get()
@@ -39,8 +52,7 @@ app.post("/scream", (req, res) => {
     createdAt: new Date().toISOString()
   };
 
-  admin
-    .firestore()
+  db.firestore()
     .collection("screams")
     .add(newScream)
     .then(doc => {
@@ -52,5 +64,57 @@ app.post("/scream", (req, res) => {
     });
 });
 
-// https://baseurl/api
-exports.api = functions.region('asia-northeast1').https.onRequest(app);
+// to register the user [signup route]
+app.post("/signup", (req, res) => {
+  const newUser = {
+    email: req.body.email,
+    password: req.body.password,
+    confirmPassword: req.body.confirmPassword,
+    handle: req.body.handle
+  };
+
+  // validating the data
+  let token, userId;
+  db.doc(`/users/${newUser.handle}`)
+    .get()
+    .then(doc => {
+      if (doc.exists) {
+        return res
+          .status(400)
+          .json({ handle: "this userhandle is already taken" });
+      } else {
+        // if the user don't exists we will register him
+        return firebase
+          .auth()
+          .createUserWithEmailAndPassword(newUser.email, newUser.password);
+      }
+    })
+    // will give a authentication token for further use of app
+    .then(data => {
+      userId = data.user.uid;
+      return data.user.getIdToken();
+    })
+    .then((idToken) => {
+      token = idToken;
+      const userCredentials = {
+        handle: newUser.handle,
+        email: newUser.email,
+        createdAt: new Date().toISOString(),
+        userId
+      };
+      return db.doc(`/users/${newUser.handle}`).set(userCredentials);
+    })
+    .then(() => {
+      return res.status(201).json({ token });
+    })
+    .catch(err => {
+      console.error(err);
+      if (err.code === "auth/email-already-in-use") {
+        return res.status(400).json({ error: "email already exists" });
+      } else {
+        return res.status(500).json({ error: err.code });
+      }
+    });
+});
+
+exports.api = functions.region("asia-northeast1").https.onRequest(app);

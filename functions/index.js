1c778932432aada64e3ed4b5da46efba1fd93de3
Aditya Mehra <aditya97mehra@gmail.com>
2019-07-29 15:13:11 +0530

    creates authentication middleware for our token

--- functions/index.js
+++ functions/index.js
@@ -44,16 +44,54 @@ app.get("/screams", (req, res) => {
     .catch(err => console.error(err));
 });
 
+// valid token authentication
+const FBAuth = (req, res, next) => {
+  let idToken;
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith('Bearer ')
+  ) {
+    idToken = req.headers.authorization.split('Bearer ')[1];
+  } else {
+    console.error("No token found");
+    return res.status(403).json({ error: "Unauthorized" });
+  }
+
+  admin
+    .auth()
+    .verifyIdToken(idToken)
+    .then(decodedToken => {
+      req.user = decodedToken;
+      // fetching the user handle
+      return db
+        .collection("users")
+        .where("userId", "==", req.user.uid)
+        .limit(1)
+        .get();
+    })
+    .then(data => {
+      req.user.handle = data.docs[0].data().handle;
+      return next();
+    })
+    .catch(err => {
+      console.error("Error while verifying token ", err);
+      return res.status(403).json(err);
+    });
+};
+
 // to create a new scream
-app.post("/scream", (req, res) => {
+app.post("/scream", FBAuth, (req, res) => {
+  if (req.body.body.trim() === "") {
+    return res.status(400).json({ body: "body must not be empty" });
+  }
+
   const newScream = {
     body: req.body.body,
-    userHandle: req.body.userHandle,
+    userHandle: req.user.handle,
     createdAt: new Date().toISOString()
   };
 
-  db.firestore()
-    .collection("screams")
+  db.collection("screams")
     .add(newScream)
     .then(doc => {
       res.json({ message: `document ${doc.id} created successfully` });

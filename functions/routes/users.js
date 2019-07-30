8afc244497c20a48935aa5d71dbb5f696fc6861e
Aditya Mehra <aditya97mehra@gmail.com>
2019-07-30 09:47:40 +0530

    refactored and organised the existent code

---
+++ functions/routes/users.js
@@ -0,0 +1,93 @@
+const firebase = require("firebase");
+const { db } = require("../util/admin");
+
+const config = require("../util/config");
+
+firebase.initializeApp(config);
+
+const { validateSignupData, validateLoginData } = require('../util/validator');
+
+exports.signup = (req, res) => {
+  const newUser = {
+    email: req.body.email,
+    password: req.body.password,
+    confirmPassword: req.body.confirmPassword,
+    handle: req.body.handle
+  };
+
+  const { valid, errors } = validateSignupData(newUser);
+  if (!valid) return res.status(400).json(errors);
+
+  // validating the data
+  let token, userId;
+  db.doc(`/users/${newUser.handle}`)
+    .get()
+    .then(doc => {
+      if (doc.exists) {
+        return res
+          .status(400)
+          .json({ handle: "this userhandle is already taken" });
+      } else {
+        // if the user don't exists we will register him
+        return firebase
+          .auth()
+          .createUserWithEmailAndPassword(newUser.email, newUser.password);
+      }
+    })
+    // will give a authentication token for further use of app
+    .then(data => {
+      userId = data.user.uid;
+      return data.user.getIdToken();
+    })
+    .then(idToken => {
+      token = idToken;
+      const userCredentials = {
+        handle: newUser.handle,
+        email: newUser.email,
+        createdAt: new Date().toISOString(),
+        userId
+      };
+      return db.doc(`/users/${newUser.handle}`).set(userCredentials);
+    })
+    .then(() => {
+      return res.status(201).json({ token });
+    })
+    .catch(err => {
+      console.error(err);
+      if (err.code === "auth/email-already-in-use") {
+        return res.status(400).json({ error: "email already exists" });
+      } else {
+        return res.status(500).json({ error: err.code });
+      }
+    });
+};
+
+exports.login = (req, res) => {
+  const user = {
+    email: req.body.email,
+    password: req.body.password
+  };
+
+  const { valid, errors } = validateLoginData(user);
+  if (!valid) return res.status(400).json(errors);
+
+  firebase
+    .auth()
+    .signInWithEmailAndPassword(user.email, user.password)
+    .then(data => {
+      return data.user.getIdToken();
+    })
+    .then(token => {
+      return res.json({ token });
+    })
+    .catch(err => {
+      console.error(err);
+      if (err.code === "auth/wrong-password") {
+        return res
+          .status(403)
+          .json({ general: "Wrong credentials : try again" });
+      } else {
+        return res.status(500).json({ error: err.code });
+      }
+    });
+};

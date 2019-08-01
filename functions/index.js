caddf4889427dd436ae472717f63c3d56d9d29a3
Aditya Mehra <aditya97mehra@gmail.com>
2019-08-01 14:56:38 +0530

    added commenting functionality on the posts

--- functions/index.js
+++ functions/index.js
@@ -5,18 +5,26 @@ const app = require("express")();
 
 const { fbAuth } = require("./util/fbAuth");
 
-const { getAllScreams, postOneScream } = require("./routes/screams");
-const { login, signup, uploadPic, addUserDetails, getAuthenticatedUser } = require("./routes/users");
+const { getAllScreams, postOneScream, getScream, createComment } = require("./routes/screams");
+const {
+  login,
+  signup,
+  uploadPic,
+  addUserDetails,
+  getAuthenticatedUser
+} = require("./routes/users");
 
 // screams route
 app.get("/screams", getAllScreams);
 app.post("/scream", fbAuth, postOneScream);
+app.get("/scream/:screamId", getScream);
+app.post('/scream/:screamId/comment', fbAuth, createComment);
 
 // new user route
 app.post("/signup", signup);
 app.post("/login", login);
 app.post("/user/pic", fbAuth, uploadPic);
 app.post("/user", fbAuth, addUserDetails);
-app.get('/user', fbAuth, getAuthenticatedUser);
+app.get("/user", fbAuth, getAuthenticatedUser);
 
 exports.api = functions.region("asia-northeast1").https.onRequest(app);

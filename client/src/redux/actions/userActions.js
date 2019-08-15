0b671a42891791fd25bf07fa3d55bf290d1dc38b
Aditya Mehra <aditya97mehra@gmail.com>
2019-08-15 18:57:39 +0530

    added logout functionality and some bugs fixed

--- client/src/redux/actions/userActions.js
+++ client/src/redux/actions/userActions.js
@@ -75,6 +75,18 @@ export const uploadImage = formData => dispatch => {
     });
 };
 
+export const editUserDetails = userDetails => dispatch => {
+  dispatch({ type: LOADING_USER });
+  axios
+    .post("/user", userDetails)
+    .then(() => {
+      dispatch(getUserData());
+    })
+    .catch(err => {
+      console.log(err);
+    });
+};
+
 const setAuthorizationHeader = token => {
   const FBIdtoken = `Bearer ${token}`;
   localStorage.setItem("FBIdToken", FBIdtoken);
